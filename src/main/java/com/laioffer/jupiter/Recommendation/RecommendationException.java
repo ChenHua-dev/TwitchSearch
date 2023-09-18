d2b58e17be7ce8a26f163897faca799e04e02a0d
Chen Hua <chua0221@gmail.com>
2023-09-18 14:33:32 -0700

    add Twitch search recommendation project to repo

---
+++ src/main/java/com/laioffer/jupiter/Recommendation/RecommendationException.java
@@ -0,0 +1,8 @@
+package com.laioffer.jupiter.Recommendation;
+
+public class RecommendationException extends RuntimeException {
+
+    public RecommendationException(String errorMessage) {
+        super(errorMessage);
+    }
+}
---
+++ src/main/java/com/laioffer/jupiter/servlet/ServletUtil.java
@@ -0,0 +1,51 @@
+package com.laioffer.jupiter.servlet;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.jupiter.entity.Item;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class ServletUtil {
+    public static <T> void writeData(HttpServletResponse response, T itemMap) throws IOException {
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().print(
+                new ObjectMapper().writeValueAsString(itemMap)
+        );
+    }
+//    public static void writeItemMap(HttpServletResponse response,
+//                                    Map<String, List<Item>> itemMap) throws IOException {
+//
+//        response.setContentType("application/json;charset=UTF-8");
+//        response.getWriter().print(
+//                new ObjectMapper().writeValueAsString(itemMap)
+//                );
+//    }
+
+    // help encrypt user password before saving to the database
+    public static String encryptPassword (
+            String userId, String password) throws IOException {
+        return DigestUtils.md5Hex(
+                userId + DigestUtils.md5Hex(password)
+        ).toLowerCase();
+
+    }
+
+    // Update LoginServlet, RegisterServlet and FavoriteServlet to use this utility function
+    public static <T> T readRequestBody(Class<T> cl, HttpServletRequest request) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(request.getReader(), cl);
+        } catch (JsonParseException | JsonMappingException e) {
+            return null;
+        }
+    }
+
+}
